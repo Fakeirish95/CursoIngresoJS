4e7e671ff5a6683835cbc8079bc79caee0a5c2c4
Fakeirish95 <40763631+Fakeirish95@users.noreply.github.com>
2018-07-02 12:08:22 -0300

    Ejercicios 1 2 y 3
    asdsad

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function mostrar()
+{
+	//var person = prompt("Please enter your name", "ugly human");
+	var nombre;
+	nombre=prompt("Ingrese su Nombre");
+	alert("Su Nombre es:"+ nombre);
+}
+
