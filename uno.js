df6cd73fb4b6daab8b8907ce13131485114c5ff6
Lucas Liker <lucasdliker@gmail.com>
2018-07-03 18:26:34 -0300

    Practica ejercicio 1
    nothig much to say

---
+++ uno.js
@@ -0,0 +1,9 @@
+
+function mostrar()
+{
+var ancho;
+var largo;
+ancho = prompt ("Ingrese el ancho del rectangulo");
+largo = prompt ("ingrese el largo del rectangulo");
+alert("el perimetro de su rectangulo es:" + ((ancho*2) + (largo*2)));
+}
