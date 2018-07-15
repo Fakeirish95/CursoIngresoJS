47dca69ea519828e1ca7b1cad19ffe8099763eb1
Lucas Liker <lucasdliker@gmail.com>
2018-07-15 13:57:32 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	//Genero el número RANDOM entre 1 y 10asd 
 	
+var numero;
 
+numero= Math.floor((Math.random() * 10) + 1);
+numero = parseInt(numero);
+
+if (numero >8)
+{alert("Excelente");
+}
+else if(numero > 3)
+{alert("aprobado");
+}
+else
+{alert("desaprobado");}
 }//FIN DE LA FUNCIÓN
