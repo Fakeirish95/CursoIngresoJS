24638938fb6668507cab7c66f4191bdf1d9cc6d0
Lucas Liker <lucasdliker@gmail.com>
2018-07-31 19:50:50 -0300

    Add files via upload

--- siete.js
+++ siete.js
@@ -3,7 +3,6 @@ ingreso:
 maria f 15
 jose m 33
 pepe m 81
-
 mostrar:
 	cantidad
 		1- de mayores de edad= 2
@@ -11,80 +10,126 @@ mostrar:
 		3-mujeres =1
 		4-cantidad de homres=2
 	nombre
-		5 del mayor edad =pepe
-		6-del menor edad=maria
-		7-del homre de menor edad= jose
-*/
+		5 del mayor edad = pepe
+		6-del menor edad = maria
+		7-del homre de menor edad = jose
+		mostrar en document.write.- */
+
 function mostrar()
 {
-
-	var nombre;
+	//variables
 	var edad;
 	var sexo;
-	var contador=0;
-	sexo=sexo.toUpperCase();	
-	var contadorMayoresEdad=0;
-	var contadorMenoresEdad=0;
-	var contadorMujeres=0;
+	var nombre;
+	var contador;
+	var contadorMayores;
+	var contadorMenores;
+	var contadorHombres;
+	var contadorMujeres;
+
+	var nombreMayor;
+	var nombreMenor;
+
+	var nombreHombreMenor;
+	var edadHombreMenor;
+
+
+	edad=parseInt(edad);
+	contador=0;
+	var contadorMayores=0;
+	var contadorMenores=0;
 	var contadorHombres=0;
+	var contadorMujeres=0;
+	var menorEdad;
+	var mayorEdad;
+	menorEdad=parseInt(menorEdad);
+	mayorEdad=parseInt(mayorEdad);
+	edadHombreMenor=parseInt(edadHombreMenor);
 
-	while (contador <3)
-	{   contador++;
-		nombre=prompt("Ingrese el nombre");
-		edad=parseInt(prompt("Ingrese la edad"));
 
-		while (edad <0 || edad > 100)
-			{
- 				edad=parseInt(prompt("Error. Ingrese la edad"));
-			}
-		
-		sexo=prompt("Ingrese el sexo");
-		sexo = sexo.toUpperCase();
+	//Validación:
 
-			while (!(sexo == "F" || sexo == "M"))
-				{
-					sexo=prompt("error. Ingrese el sexo");
-					sexo = sexo.toUpperCase();				
-				}	
+		while (contador < 3)
+		{
+			nombre=prompt("Ingrese el nombre");
+			edad=prompt("Ingrese la edad, de 0 a 100");
+			edad=parseInt(edad);
+			sexo=prompt("Ingrese el sexo f o m");
+			sexo = sexo.toLowerCase();
+			contador++
 
+	//algorismos
+				
+				while (edad < 0 || edad > 100)
+					{
+						edad=prompt("Ingrese una edad valida de 0 a 100");
+						edad=parseInt(edad);
+					}
 					if (edad > 17)
-					{
-						contadorMayoresEdad++
-					}
+						{
+							contadorMayores++;
+						
+						}
+						else if (edad < 17)
+							{
+								contadorMenores++;
+								
+							}
+							if (sexo == "f")
+								{
+									contadorMujeres++;
+								}			
+								else if (sexo == "m")
+									{
+										contadorHombres++;
+									}								
+									 if (contador == 1)
+										{
+											menorEdad = edad;
+											mayorEdad = edad;
+										}
 
-					else if (edad < 17)
-					{
-						contadorMenoresEdad++
-					}
-
-					else if (sexo == "F")
-					{
-						contadorMujeres++
-					}
-					else if (sexo =="M")
-					{
-						contadorHombres++
-					}
-
+										else if (mayorEdad < edad)
+											{
+												mayorEdad=edad;
+												nombreMayor=nombre;												
+											}										
+											else if (menorEdad > edad)
+												{
+													menorEdad=edad;
+													nombreMenor=nombre;													
+												}
+												if (sexo = "m" && )
+												 
 
 
+		}
 
-
-				
-
-				
-
-
+if (nombreMayor=nombre && sexo = "m")
+{
+edadHombreMenor=edad;
+nombreHombreMenor=nombre;
+}	
 
 
 
 
 
 
-	}
 
-document.write("la cantidad de personas mayores de edad es " + contadorMayoresEdad + "<br>");
-document.write("la cantidad de personas menores de edad es " + contadorMenoresEdad + "<br>");
+
+//Mostrar
+
+document.write("La cantidad de mayores de edad es de " + contadorMayores + " personas" + "<br>");
+document.write("La cantidad de menores de edad es de " + contadorMenores + "personas" + "<br>");
+document.write("La cantidad de hombres es de " + contadorHombres + "<br>");
+document.write ("La cantidad de mujeres es de " + contadorMujeres + "<br>");
+document.write("El nombre del menor de edad es " +nombreMenor+ " y tiene " + menorEdad + " años." + "<br>" );
+document.write("el nombre del mayor de edad es " +nombreMayor+ " y tiene " + mayorEdad +  " años" +"<br>");
+document.write("El hombre menor es " + nombreHombreMenor + " y tiene " + edadHombreMenor + " años" + "<br>");
+
+
+
 
 
 }
